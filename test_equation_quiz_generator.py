517d8ee5de46547c85ed000b8d6cd0339fda3840
Sameerah <123633335+sameerahmahmood@users.noreply.github.com>
2025-01-22 20:31:52 +0000

    Update test_equation_quiz_generator.py

--- test_equation_quiz_generator.py
+++ test_equation_quiz_generator.py
@@ -2,13 +2,17 @@ import pytest
 from equation_quiz_generator import check_user_score
     
 def test_check_user_score():
-    #correct user case
+    """ testing the check_user_score function to ensure that the score and streak are correct"""
+    #correct user answer case
     user_score, user_streak = check_user_score(10,10,0,0)
     assert user_score == 1
     assert user_streak == 1
-    
+    #incorrect user answer case
     user_score, user_streak = check_user_score(7,3,1,1)
     assert user_score == 1
     assert user_streak == 0
+    
+
+
     
 
